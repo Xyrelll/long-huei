22ee1559243cfda2654e67cedb4585ad3e16bb21
Xyrelll <ella587620@gmail.com>
2025-12-24 15:02:28 +0800

    UPDATE: Latest articles section

--- src/components/features/Hero/Hero.tsx
+++ src/components/features/Hero/Hero.tsx
@@ -2,7 +2,6 @@
 
 import Image from 'next/image';
 import Link from 'next/link';
-// @ts-ignore
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import '@splidejs/splide/dist/css/splide.min.css';
 
