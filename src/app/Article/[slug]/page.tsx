f7a3074369bd06910012030e39a9b53876696f87
Xyrelll <ella587620@gmail.com>
2025-12-27 15:52:34 +0800

    UPDATE: Article count

--- src/app/Article/[slug]/page.tsx
+++ src/app/Article/[slug]/page.tsx
@@ -28,7 +28,7 @@ interface Article {
 }
 
 const categories = [
-  { name: "旅遊", href: "/ArticleCategory/Travel", count: 11 },
+  { name: "旅遊", href: "/ArticleCategory/Travel", count: 12 },
   { name: "桑拿", href: "/ArticleCategory/Sauna", count: 11 },
   { name: "包車", href: "/ArticleCategory/RentCar", count: 12 },
   { name: "訂房", href: "/ArticleCategory/Booking", count: 5 },
